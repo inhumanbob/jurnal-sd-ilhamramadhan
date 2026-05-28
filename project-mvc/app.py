000d9564ed691059345f6e18d2a83017c934d111
ilham ramadhan <ilhamdimas231@gmail.com>
2026-05-28 16:00:04 +0700

    [CHORE] Integrasi awal frontend-backend secara lokal

--- project-mvc/app.py
+++ project-mvc/app.py
@@ -1,23 +1,8 @@
-import time
-from views.dashboard_component import render_dashboard
-
-# Simulasi State
-app_state = {"items": [], "is_loading": True}
-
-def update_state(new_data):
-    app_state["items"] = new_data
-    app_state["is_loading"] = False
+from controllers.api_handler import get_users
+from views.dashboard_component import fetch_data_from_api, render_dashboard
 
 if __name__ == "__main__":
-    # 1. Menampilkan state awal (Data belum ada, is_loading = True)
-    render_dashboard(app_state["items"], app_state["is_loading"])
-    
-    print("\n[Sistem] Mengambil data dari Backend...\n")
-    time.sleep(2) # Memberikan jeda 2 detik seolah-olah sedang loading
-    
-    # 2. Simulasi data masuk dari Backend
-    mock_data = [{"id": 101, "name": "Produk A"}, {"id": 102, "name": "Produk B"}]
-    update_state(mock_data)
-    
-    # 3. Menampilkan state akhir (Data sudah ada, is_loading = False)
-    render_dashboard(app_state["items"], app_state["is_loading"])
+    # Proses Integrasi
+    data = fetch_data_from_api(get_users)
+    if data:
+        render_dashboard(data)
