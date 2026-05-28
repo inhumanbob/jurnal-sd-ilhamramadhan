63c61d79f2016d077a28cf75fcdce0bd1adbe7d3
ilham ramadhan <ilhamdimas231@gmail.com>
2026-05-28 19:54:59 +0700

    [FEAT] Implementasi Named Volume untuk persistensi data

--- project-mvc/app.py
+++ project-mvc/app.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 # Membaca variabel dari environment
 user_name = os.getenv('APP_USER', 'Guest')
@@ -6,3 +7,8 @@ app_env = os.getenv('APP_ENV', 'development')
 
 if __name__ == "__main__":
     print(f"Halo {user_name}! Aplikasi ini berjalan di dalam kontainer Docker dengan status lingkungan '{app_env}'.")
+    print("[System] Menjaga kontainer tetap hidup untuk pengujian volume...")
+    
+    # Loop ini membuat program Python standby terus menerus
+    while True:
+        time.sleep(3600)
